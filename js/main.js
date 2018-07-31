7ee548398b0ffe9c51a3621d2e39ca85a4c6d767
Bailey Charlton <bbcharlton.web@gmail.com>
2018-07-31 14:58:38 -0400

    Styling for main menu as well as some structure edits.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 // Place your custom JavaScript here.
 window.onload = function() {
+  var main_menu = document.getElementsByClassName('main-menu-item');
+  var pathArray = window.location.pathname.split( '/' );
 
-  
+  // Sets the active class for the main menu since Drupal's way
+  // of doing so seems to be broken.
+  for (var i = 0; i < main_menu.length; i++) {
+    if (main_menu[i].getAttribute('href') == ('/' + pathArray[1])) {
+      main_menu[i].classList.add('active');
+    }
+  }
 };
